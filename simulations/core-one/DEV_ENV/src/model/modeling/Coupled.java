062753b501f7767b3680754eef6395f4c23bb44f
ndebuhr <ndebuhr@protonmail.com>
2018-01-01 20:59:19 -0500

    Clean up architecture simulation files

--- simulations/core-one/DEV_ENV/src/model/modeling/Coupled.java
+++
@@ -1,9 +0,0 @@
-/* 
- * Author     : ACIMS(Arizona Centre for Integrative Modeling & Simulation)
- *  Version    : DEVSJAVA 2.7 
- *  Date       : 08-15-02 
- */ 
-
-package model.modeling;
-
-public interface Coupled extends coupledDevs,IOBasicDevs{}
