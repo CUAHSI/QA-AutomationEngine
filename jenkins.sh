3b71e87004f9e976e73ecc36655f1e176c8aa01a
ndebuhr <ndebuhr@gmail.com>
2018-01-19 15:54:33 -0500

    Attempted fix for jenkins thread allocation issue

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,8 @@
-i=0 # i controls which thread to run test on
+i="-1" # i controls which thread to run test on
 while read TEST; do
     while true; do
+	i=$(($i + 1))
+	i=$(($i % 8)) # assumes 8 threads available
 	CRUMB=$(curl -k -s "http://$JENKINSIP:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" --user "$USERNAME":"$USERPASS")
 	curl -X GET "http://$JENKINSIP:8080/job/$TESTSUITE-thread$i/lastBuild/api/xml?depth=1&token=$APITOKEN" -k -H "$CRUMB" --user "$USERNAME":"$USERPASS" > thread.json
 	THREAD=$(grep -c '<building>false</building>' thread.json)
@@ -11,8 +13,6 @@ while read TEST; do
 	else
 	    echo "Thread $i Not Available"
 	    sleep 1
-	    i=$(($i + 1))
-	    i=$(($i % 8)) # assumes 8 threads available
 	fi
 	sleep 1
     done
