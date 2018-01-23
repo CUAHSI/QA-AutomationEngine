284e0704659e6203f57844b2413d7caea5dd0474
ndebuhr <ndebuhr@gmail.com>
2018-01-23 08:48:35 -0500

    Remove old models from simulation workspace

--- simulations/core-one/DEVS_Suite_3.0.0_executable_linux_64/update_jar.sh
+++ simulations/core-one/DEVS_Suite_3.0.0_executable_linux_64/update_jar.sh
@@ -1,15 +1,11 @@
 cp src/CoreOne/*.class bin/CoreOne/
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/SeleniumGridNode.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/SeleniumGridHub.class
-jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/TestingEngine.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/Jenkins.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/GithubRepo.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/JenkinsCoord.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/NealEmail.class
-jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/SimpleDjangoSite.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/JenkinsJobTestcase.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/GitHub.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/JenkinsOnline.class
-jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/Empty.class
-jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/EmptyUnit.class
 jar uf DEVS_Suite_3.0.0.jar -C bin CoreOne/TestingEngineProgress.class
