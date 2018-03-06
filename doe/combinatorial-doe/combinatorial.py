db7634046fa74bf1b2740d819fb4756a253be454
ndebuhr <ndebuhr@protonmail.com>
2018-03-05 22:14:25 -0500

    Refactor combinatorial utility

--- doe/combinatorial-doe/combinatorial.py
+++ doe/combinatorial-doe/combinatorial.py
@@ -7,126 +7,112 @@ from random import randint
 
 # Arguments parsing
 parser = argparse.ArgumentParser()
+parser.add_argument('--experiments')
 parser.add_argument('--factors')
-parser.add_argument('--goal')
-parser.add_argument('--spec')
+parser.add_argument('--specification')
 args = parser.parse_args()
 
 
 def parse_args(args):
     """ Parse spec argument and convert to integers """
-    depths = args.spec.split(',')
+    depths = args.specification.split(',')
     depths = [int(i) for i in depths]
-    num_experiments = int(args.goal)
-    num_factors = int(args.factors)
-    return depths, num_experiments, num_factors
+    experiments_count = int(args.experiments)
+    factors = int(args.factors)
+    return depths, experiments_count, factors
 
 
-def validate_arguments(depths, num_experiments, num_factors):
-    """ Confirm goal is theoretically possible.  Confirm length of
-    spec is long enough, given number of factors.
+def validate_arguments(depths, experiments_count, factors):
+    """ Confirm number of experiments is theoretically possible.  Confirm
+    length of specification is long enough, given number of factors.
     """
     # Confirm number of factors is equal to or less than spec length
     # Note: number of factors equal to spec length is just full combinatorial
     assert(len(depths) >= int(args.factors))
 
     # Establish the highest two values in the spec
-    depths_copy = [i for i in depths]
-    depth_max_one = max(depths_copy)  # largest number in depths_copy
-    depths_copy.pop(depths_copy.index(depth_max_one))  # remove largest
-    depth_max_two = max(depths_copy)  # second largest number in depths_copy
+    depths_cp = [i for i in depths]
+    depth_max = [max(depths_cp)]  # largest number in depths copy
+    depths_cp.pop(depths_cp.index(depth_max[0]))  # remove largest
+    depth_max += [max(depths_cp)]  # second largest number in depths copy
 
     # Confirm goal is theoretically possible
-    theoretical_min_experiments = depth_max_one * depth_max_two
-    assert(num_experiments >= theoretical_min_experiments)
+    experiments_min = depth_max[0] * depth_max[1]
+    assert(experiments_count >= experiments_min)
 
 
-def create_random_experiments(depths, num_experiments):
+def create_random_experiments(depths, experiments_count):
     """ Create random set of experiments """
-    experiments_set = []
-    for i in range(0, num_experiments):
+    experiments = []
+    for i in range(0, experiments_count):
         experiment = []
         for j in range(0, len(depths)):
             experiment.append(randint(0, depths[j]-1))
-        experiments_set.append(experiment)
-    return experiments_set
+        experiments.append(experiment)
+    return experiments
 
+def combinations_met(depths, experiments, factors):
+    combinations = []
+    combinations_needed = 1
+    for n in range(0, len(depths)):
+        combinations_needed *= depths[n]
+    for p in range(0, len(experiments)):
+        combination = []
+        for q in range(0, factors):
+            combination.append(experiments[p][q])
+        if combination not in combinations:
+            combinations.append(combination)
+    if len(combinations) != combinations_needed:
+        return False
+    else:
+        return True
+    
 
-def check_two_way(depths, experiments):
+def passes_two_way(depths, experiments):
     # Two way check
     for i in range(0, len(depths)-1):
         for j in range(i+1, len(depths)):
-            combinations = []
-            combinations_needed = depths[i] * depths[j]
-            for k in range(0, len(experiments)):
-                if (experiments[k][i], experiments[k][j]) \
-                   not in combinations:
-                    combinations.append((experiments[k][i],
-                                         experiments[k][j]))
-            if len(combinations) != combinations_needed:
+            if not combinations_met(depths, experiments, factors):
                 return False
     return True
 
 
-def check_three_way(depths, experiments):
+def passes_three_way(depths, experiments):
     # Three way check
     for i in range(0, len(depths)-2):
         for j in range(i+1, len(depths)-1):
             for k in range(i+2, len(depths)):
-                combinations = []
-                combinations_needed = depths[i] * depths[j] * depths[k]
-                for l in range(0, len(experiments)):
-                    if (experiments[l][i],
-                        experiments[l][j],
-                        experiments[l][k]) \
-                       not in combinations:
-                        combinations.append((experiments[l][i],
-                                             experiments[l][j],
-                                             experiments[l][k]))
-                if len(combinations) != combinations_needed:
+                if not combinations_met(depths, experiments, factors):
                     return False
     return True
 
 
-def check_four_way(depths, experiments):
+def passes_four_way(depths, experiments):
     # Four way check
     for i in range(0, len(depths)-3):
         for j in range(i+1, len(depths)-2):
             for k in range(i+2, len(depths)-1):
                 for l in range(i+3, len(depths)):
-                    combinations = []
-                    combinations_needed = depths[i] * depths[j] * \
-                        depths[k] * depths[l]
-                    for m in range(0, len(experiments)):
-                        if (experiments[m][i],
-                            experiments[m][j],
-                            experiments[m][k],
-                            experiments[m][l]) \
-                           not in combinations:
-                            combinations.append((experiments[m][i],
-                                                 experiments[m][j],
-                                                 experiments[m][k],
-                                                 experiments[m][l]))
-                    if len(combinations) != combinations_needed:
+                    if not combinations_met(depths, experiments, factors):
                         return False
     return True
 
 
 # Parse and validate user-provided arguments
-depths, num_experiments, num_factors = parse_args(args)
-validate_arguments(depths, num_experiments, num_factors)
+depths, experiments_count, factors = parse_args(args)
+validate_arguments(depths, experiments_count, factors)
 
 attempts = 0
 solved = False
 while not solved:
-    experiments = create_random_experiments(depths, num_experiments)
-    assert(2 <= num_factors and num_factors <= 4)
-    if num_factors == 2:
-        solved = check_two_way(depths, experiments)
-    elif num_factors == 3:
-        solved = check_three_way(depths, experiments)
-    elif num_factors == 4:
-        solved = check_four_way(depths, experiments)
+    experiments = create_random_experiments(depths, experiments_count)
+    assert(2 <= factors and factors <= 4)
+    if factors == 2:
+        solved = passes_two_way(depths, experiments)
+    elif factors == 3:
+        solved = passes_three_way(depths, experiments)
+    elif factors == 4:
+        solved = passes_four_way(depths, experiments)
     if solved:
         print('Solution Found:')
         for i in range(0, len(experiments)):
