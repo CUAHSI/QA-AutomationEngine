be53527278f0abb24470b49ea90b3d733105d967
ndebuhr <ndebuhr@protonmail.com>
2018-03-11 23:37:54 -0400

    Create foundation for language checking on pages visited by the test suites

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ class External:
         win_handle = driver.window_handles[-1]
         driver.switch_to.window(win_handle)
         time.sleep(SLEEP_TIME)
+        TestSystem.check_language(driver)
 
     def switch_old_page(self, driver):
         win_handle = driver.window_handles[-2]
@@ -39,6 +40,7 @@ class External:
         driver.switch_to.window(new_handle)
         time.sleep(SLEEP_TIME/2)
         source = driver.page_source
+        TestSystem.check_language(driver)
         driver.switch_to.window(orig_handle)
         time.sleep(SLEEP_TIME/2)
         return source
@@ -56,7 +58,41 @@ class TestSystem:
         return driver.page_source
 
     def back(self, driver):
+        self.check_language(driver)
         driver.execute_script("window.history.go(-1)")
+
+    def pull_texts(self, driver):
+        elements = driver.find_elements_by_xpath('*')
+        texts = []
+        for element in elements:
+            if element.text is not None:
+                texts += element.text.split(' ')
+        return texts
+
+    def check_language(self, driver):
+        """ Confirms that language is consistent with CUAHSI standards for
+        terminology, capitalization, etc.
+        """
+        lang_dict = {}
+        with open("config/language.yaml", 'r') as stream:
+            lines = stream.readlines()
+        for line in lines:
+            if line[0:3] != '###':
+                lang_dict[line.split(':')[0].strip()] = (
+                    line.split(':')[1].strip())
+        pulled_words = self.pull_texts(driver)
+        words = []
+        for pulled_word in pulled_words:
+            words += pulled_word.split('\n')
+        words = list(filter(None, words))
+        words = [word.replace('.', '') for word in words]
+        words = [word.replace(',', '') for word in words]
+        words = [word.replace('(', '') for word in words]
+        words = [word.replace(')', '') for word in words]
+        for word in words:
+            if word in lang_dict.keys():
+                print('\n', driver.current_url, '\n',
+                      word + ' should be changed to ' + lang_dict[word])
 
 
 External = External()
