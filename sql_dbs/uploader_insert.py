7b798a70a3eee1932812d109f135b25cfc68ecad
ndebuhr <ndebuhr@protonmail.com>
2018-03-25 19:18:34 -0400

    Increase script performance through metadata tables caching and batching inserts

--- sql_dbs/uploader_insert.py
+++ sql_dbs/uploader_insert.py
@@ -10,6 +10,7 @@ parser.add_argument('--sources')
 parser.add_argument('--variables')
 args = parser.parse_args()
 
+batch_size = 500
 
 def get_csv_data(csv_name):
     with open(csv_name, 'r') as csv_file:
@@ -24,11 +25,17 @@ def convert_to_list(csv_line):
     return csv_list
 
 
-def convert_to_id(cursor, table, cv_label, cv_value, id_label):
-    cursor.execute("""
-    SELECT {} FROM {} WHERE {}='{}';
-    """.format(id_label, table, cv_label, cv_value))
-    return cursor.fetchone()[0]
+def pull_metadata_table(cursor, table):
+    cursor.execute("SELECT * FROM {};".format(table))
+    columns = [column[0] for column in cursor.description]
+    return [tuple(columns)] + cursor.fetchall()
+
+def convert_to_id(cursor, metadata_caches, table, cv_label, cv_value, id_label):
+    table = metadata_caches[table]
+    cv_col = table[0].index(cv_label)
+    id_col = table[0].index(id_label)
+    target_row = [i for i in range(0, len(table)) if table[i][cv_col] == cv_value][0]
+    return table[target_row][id_col]
 
 
 def format_fields(fields, nvarchars):
@@ -41,17 +48,17 @@ def format_fields(fields, nvarchars):
     return formatted_fields
 
 
-def post_datavalues(cursor, datavalues_csv):
+def post_datavalues(cursor, datavalues_csv, metadata_caches):
     for ind, datavalue_csv in enumerate(datavalues_csv, start=1):
         datavalue_csv = convert_to_list(datavalue_csv)
-        methodid = convert_to_id(cursor, 'Methods', 'METHODCODE',
-                                 datavalue_csv[8], 'METHODID')
-        siteid = convert_to_id(cursor, 'Sites', 'SITECODE',
-                               datavalue_csv[0], 'SITEID')
-        sourceid = convert_to_id(cursor, 'Sources', 'SOURCECODE',
-                                 datavalue_csv[9], 'SOURCEID')
-        variableid = convert_to_id(cursor, 'Variables', 'VARIABLECODE',
-                                   datavalue_csv[1], 'VARIABLEID')
+        methodid = convert_to_id(cursor, metadata_caches, 'methods', 'MethodCode',
+                                 datavalue_csv[8], 'MethodID')
+        siteid = convert_to_id(cursor, metadata_caches, 'sites', 'SiteCode',
+                               datavalue_csv[0], 'SiteID')
+        sourceid = convert_to_id(cursor, metadata_caches, 'sources', 'SourceCode',
+                                 datavalue_csv[9], 'SourceID')
+        variableid = convert_to_id(cursor, metadata_caches, 'variables',
+                                   'VariableCode', datavalue_csv[1], 'VariableID')
         datavalue_db = [datavalue_csv[2],  # datavalue
                         datavalue_csv[3],  # localdatetime
                         datavalue_csv[4],  # utcoffset
@@ -64,14 +71,21 @@ def post_datavalues(cursor, datavalues_csv):
                         datavalue_csv[6]  # qualitycontrollevelid
                         ]
         nvarchars = [1, 3, 6]
-        datavalue_labels = ['DATAVALUE', 'LOCALDATETIME', 'UTCOFFSET', 'DATETIMEUTC',
-                            'SITEID', 'VARIABLEID', 'CENSORCODE',
-                            'METHODID', 'SOURCEID',
-                            'QUALITYCONTROLLEVELID']
         datavalue_db = format_fields(datavalue_db, nvarchars)
-        cursor.execute('INSERT INTO DataValues ({}) VALUES ({});'.format(
-            ', '.join(datavalue_labels), ', '.join(datavalue_db)))
-    cursor.commit()
+        if ind % batch_size == 1:
+            datavalue_labels = ['DATAVALUE', 'LOCALDATETIME',
+                                'UTCOFFSET', 'DATETIMEUTC',
+                                'SITEID', 'VARIABLEID', 'CENSORCODE',
+                                'METHODID', 'SOURCEID',
+                                'QUALITYCONTROLLEVELID']
+            insert_cmd = 'INSERT INTO DataValues ({}) VALUES ({})'.format(
+                ', '.join(datavalue_labels), ', '.join(datavalue_db))
+        else:
+            insert_cmd += ', ({})'.format(', '.join(datavalue_db))
+        if ind % batch_size == 0 or ind == len(datavalues_csv):
+            insert_cmd += ';'
+            cursor.execute(insert_cmd)
+            cursor.commit()
 
 
 def post_methods(cursor, methods_csv):
@@ -85,6 +99,31 @@ def post_methods(cursor, methods_csv):
         method_db = format_fields(method_db, nvarchars)
         cursor.execute('INSERT INTO Methods ({}) VALUES ({});'.format(
             ', '.join(method_labels), ', '.join(method_db)))
+    cursor.commit()
+
+
+def post_sites(cursor, sites_csv, metadata_caches):
+    for ind, site_csv in enumerate(sites_csv, start=1):
+        site_csv = convert_to_list(site_csv)
+        latlongdatumid = convert_to_id(cursor, metadata_caches, 'spatialreferences',
+                                       'SRSName', site_csv[6], 'SpatialReferenceID')
+        site_db = [site_csv[0],  # sitecode
+                   site_csv[1],  # sitename
+                   site_csv[2],  # latitude
+                   site_csv[3],  # longitude
+                   latlongdatumid,  # latlongdatumid
+                   site_csv[7],  # elevation_m
+                   site_csv[5],  # state
+                   site_csv[4],  # county
+                   site_csv[8]  # sitetype
+                   ]
+        nvarchars = [0, 1, 6, 7, 8]
+        site_labels = ['SITECODE', 'SITENAME', 'LATITUDE', 'LONGITUDE',
+                       'LATLONGDATUMID', 'ELEVATION_M', 'STATE',
+                       'COUNTY', 'SITETYPE']
+        site_db = format_fields(site_db, nvarchars)
+        cursor.execute('INSERT INTO Sites ({}) VALUES ({});'.format(
+            ', '.join(site_labels), ', '.join(site_db)))
     cursor.commit()
 
 
@@ -116,40 +155,13 @@ def post_sources(cursor, sources_csv):
     cursor.commit()
 
 
-def post_sites(cursor, sites_csv):
-    for ind, site_csv in enumerate(sites_csv, start=1):
-        site_csv = convert_to_list(site_csv)
-        latlongdatumid = convert_to_id(cursor, 'SpatialReferences', 'SRSNAME',
-                                       site_csv[6], 'SpatialReferenceID')
-        site_db = [site_csv[0],  # sitecode
-                   site_csv[1],  # sitename
-                   site_csv[2],  # latitude
-                   site_csv[3],  # longitude
-                   latlongdatumid,  # latlongdatumid
-                   site_csv[7],  # elevation_m
-                   site_csv[5],  # state
-                   site_csv[4],  # county
-                   site_csv[8]  # sitetype
-                   ]
-        nvarchars = [0, 1, 6, 7, 8]
-        site_labels = ['SITECODE', 'SITENAME', 'LATITUDE', 'LONGITUDE',
-                       'LATLONGDATUMID', 'ELEVATION_M', 'STATE',
-                       'COUNTY', 'SITETYPE']
-        site_db = format_fields(site_db, nvarchars)
-        print('INSERT INTO Sites ({}) VALUES ({});'.format(
-            ', '.join(site_labels), ', '.join(site_db)))
-        cursor.execute('INSERT INTO Sites ({}) VALUES ({});'.format(
-            ', '.join(site_labels), ', '.join(site_db)))
-    cursor.commit()
-
-
-def post_variables(cursor, variables_csv):
+def post_variables(cursor, variables_csv, metadata_caches):
     for ind, variable_csv in enumerate(variables_csv, start=1):
         variable_csv = convert_to_list(variable_csv)
-        variableunitsid = convert_to_id(cursor, 'Units', 'UNITSNAME',
-                                        variable_csv[2], 'UNITSID')
-        timeunitsid = convert_to_id(cursor, 'Units', 'UNITSNAME',
-                                    variable_csv[8], 'UNITSID')
+        variableunitsid = convert_to_id(cursor, metadata_caches, 'units',
+                                        'UnitsName', variable_csv[2], 'UnitsID')
+        timeunitsid = convert_to_id(cursor, metadata_caches, 'units', 'UnitsName',
+                                    variable_csv[8], 'UnitsID')
         variable_db = [variable_csv[0],  # variablecode
                        variable_csv[1],  # variablename
                        'Not applicable',  # speciation
@@ -184,19 +196,30 @@ cnxn = pyodbc.connect('DRIVER={};PORT={};'.format(driver, 1433) +
                       'DATABASE={};UID={};PWD={};'.format(db_name, user, passwd))
 cursor = cnxn.cursor()
 
+spatial_references = pull_metadata_table(cursor, 'SpatialReferences')
+metadata_caches = {'spatialreferences':spatial_references}
+
 if args.methods is not None:
     methods = get_csv_data(args.methods)
     post_methods(cursor, methods)
 if args.sites is not None:
     sites = get_csv_data(args.sites)
-    post_sites(cursor, sites)
+    post_sites(cursor, sites, metadata_caches)
 if args.sources is not None:
     sources = get_csv_data(args.sources)
     post_sources(cursor, sources)
+
+metadata_caches['methods'] = pull_metadata_table(cursor, 'Methods')
+metadata_caches['sites'] = pull_metadata_table(cursor, 'Sites')
+metadata_caches['sources'] = pull_metadata_table(cursor, 'Sources')
+metadata_caches['units'] = pull_metadata_table(cursor, 'Units')
+
 if args.variables is not None:
     variables = get_csv_data(args.variables)
-    post_variables(cursor, variables)
+    post_variables(cursor, variables, metadata_caches)
+
+metadata_caches['variables'] = pull_metadata_table(cursor, 'Variables')
 
 if args.datavalues is not None:
     datavalues = get_csv_data(args.datavalues)
-    post_datavalues(cursor, datavalues)
+    post_datavalues(cursor, datavalues, metadata_caches)
