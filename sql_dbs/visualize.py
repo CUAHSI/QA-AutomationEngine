f664dbac1aea5719a2022a6480ecad942402e535
ndebuhr <ndebuhr@protonmail.com>
2018-04-21 23:11:28 +0200

    Enable multi-trial data analysis for both linear plots and heatmap plot

--- sql_dbs/visualize.py
+++ sql_dbs/visualize.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 import pandas as pd
 import seaborn as sns
 
@@ -47,8 +48,7 @@ def create_linear_graphs(df, indep_names, dep_name, output_folder):
     """
     Groups dataset by each of two independent variables (thus, 'fixing' 1 variable)
     and plots a relation between other (non-fixed) independent variable
-    and dependent one.
-    Resulting graphs are saved into specified output directory.
+    and dependent one.  Resulting graphs are saved into specified output directory.
     """
     sns.set()
     graph_number = 1
@@ -58,48 +58,52 @@ def create_linear_graphs(df, indep_names, dep_name, output_folder):
         for value in fixed_values:
             group = df[df[fixed_name] == value].sort_values(other_name)
 
-            plt.figure()
-            plt.plot(other_name, dep_name, 'bo', data=group)
-            plt.xlabel(other_name)
-            plt.ylabel(label_format(dep_name))
+            plt.figure(figsize=(8, 8), dpi=160)
+            sns.lmplot(other_name, dep_name, data=group, fit_reg=False)
+            plt.xlabel(other_name, fontsize=10)
+            plt.ylabel(label_format(dep_name), fontsize=10)
             plt.title('{} = {}'.format(fixed_name, value))
 
             output_file = os.path.join(output_folder,
                                        'fixed-{}.png'.format(graph_number))
+            plt.tick_params(axis='both', which='major', labelsize=7)            
             plt.savefig(output_file)
             graph_number += 1
 
 
 def create_heatmaps(df, indep_names, dep_name, output_folder):
     """
-    Plots a relation between two independent variables and a dependent
-    one in a heatmap.
-    Resulting graphs are saved into specified output directory.
+    Plots a relation between two independent variables and a dependent one in a
+    heatmap.  Resulting graphs are saved into specified output directory.
     """
     sns.set()
     target_columns = indep_names + [dep_name]
     x1, x2, y = target_columns
     df_heat = df[target_columns].sort_values(target_columns)
-    # setup color palette
+    # sort data into independent variable configurations, then assign single
+    # value to configuration as mean
+    df_heat = df_heat.groupby(indep_names, as_index=False)[dep_name].mean()
     colors_count = len(df_heat[dep_name].unique())
     palette = sns.cubehelix_palette(colors_count, start=2.5, rot=1., light=0.8)
-    title = '{y}({x1},{x2})'.format(y=y, x1=x1, x2=x2)
+    title = '{y}({x1},{x2}) (average in seconds)'.format(y=y, x1=x1, x2=x2)
 
     # 'pivot'ed heatmap
     pivot = df_heat.pivot(*target_columns)
-    plt.figure()
-    sns.heatmap(pivot, annot=True, cmap=palette)
-    plt.xlabel(label_format(x2))
-    plt.ylabel(label_format(x1))
+    plt.figure(figsize=(8, 8), dpi=160)
+    sns.heatmap(pivot, annot=True, fmt='g', cmap=palette)
+    plt.xlabel(x2, fontsize=10)
+    plt.ylabel(x1, fontsize=10)
     plt.title(title)
+    plt.tick_params(axis='both', which='major', labelsize=7)
     plt.savefig(os.path.join(output_folder, 'heatmap-1.png'))
 
     # table-like heatmap
-    plt.figure()
-    df_heat = df_heat.rename(columns={x1: label_format(x1), x2: label_format(x2)})
-    sns.heatmap(df_heat, yticklabels=False, annot=True, annot_kws={"size": 9},
-                cmap=palette)
+    plt.figure(figsize=(8, 8), dpi=160)
+    df_heat = df_heat.rename(columns={x1: x1, x2: x2})
+    sns.heatmap(df_heat, yticklabels=False, annot=True, fmt='g',
+                annot_kws={"size": 9}, cmap=palette)
     plt.title(title)
+    plt.tick_params(axis='both', which='major', labelsize=7)
     plt.savefig(os.path.join(output_folder, 'heatmap-2.png'))
 
 
@@ -112,6 +116,7 @@ def main():
     independent_var_names = args.independent_vars
     dependent_var_name = args.dependent_var
 
+    rcParams.update({'figure.autolayout': True})
     create_linear_graphs(df, independent_var_names, dependent_var_name,
                          output_folder)
     create_heatmaps(df, independent_var_names, dependent_var_name,
