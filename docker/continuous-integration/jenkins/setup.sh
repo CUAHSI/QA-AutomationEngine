4d04d1819268fcb97df8a9311e41b8ac14983ba7
ndebuhr <ndebuhr@protonmail.com>
2018-09-09 22:09:48 +0000

    Use absolute path for setup shell script

--- docker/continuous-integration/jenkins/setup.sh
+++ docker/continuous-integration/jenkins/setup.sh
@@ -1,3 +1,3 @@
 INITPASS=$(cat /var/jenkins_home/secrets/initialAdminPassword) \
 && CRUMB=$(curl -k -s "http://localhost:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" --user "admin":"$INITPASS") \
-&& curl -s -X POST "http://localhost:8080/createItem?name=command-core" -k -H "$CRUMB" --user "admin":"$INITPASS" --data-binary "@jobs/command-core.xml" -H "Content-Type:text/xml"
+&& curl -s -X POST "http://localhost:8080/createItem?name=command-core" -k -H "$CRUMB" --user "admin":"$INITPASS" --data-binary "@/var/jenkins_home/cuahsi/jenkins/jobs/command-core.xml" -H "Content-Type:text/xml"
