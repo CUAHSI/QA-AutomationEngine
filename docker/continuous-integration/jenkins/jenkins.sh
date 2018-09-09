857f1020f6066f76b245ceb5a8fec0972574ae6c
ndebuhr <ndebuhr@protonmail.com>
2018-09-09 22:55:07 +0000

    Setup parameterized job config details

--- docker/continuous-integration/jenkins/jenkins.sh
+++ docker/continuous-integration/jenkins/jenkins.sh
@@ -1,9 +1,10 @@
+USERPASS=$(cat /var/jenkins_home/secrets/initialAdminPassword)
 while read TEST; do
     CRUMB=$(curl -k -s "http://$JENKINSIP:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)" --user "$USERNAME":"$USERPASS")
-    if curl -s -X GET -k -H "$CRUMB" --user "$USERNAME":"$USERPASS" --output /dev/null --head --fail "http://$JENKINSIP:8080/job/$TESTSUITE-$TEST/lastBuild/api/xml?depth=1&token=$APITOKEN"; then
+    if curl -s -X GET -k -H "$CRUMB" --user "$USERNAME":"$USERPASS" --output /dev/null --head --fail "http://$JENKINSIP:8080/job/$TESTSUITE-$TEST/lastBuild/api/xml?depth=1"; then
 	echo "$TESTSUITE-$TEST exists.  Starting job."
 	sleep 1
-	curl -s -X POST "http://$JENKINSIP:8080/job/$TESTSUITE-$TEST/buildWithParameters?delay=0sec&TESTCASE=$TEST&token=$APITOKEN" -k -H "$CRUMB" --user "$USERNAME":"$USERPASS"
+	curl -s -X POST "http://$JENKINSIP:8080/job/$TESTSUITE-$TEST/buildWithParameters?delay=0sec&$TESTCASE=$TEST" -k -H "$CRUMB" --user "$USERNAME":"$USERPASS"
     else
 	echo "$TESTSUITE-$TEST does not exist.  Creating, then starting job."
 	sleep 1
@@ -11,7 +12,7 @@ while read TEST; do
 	sleep 1
 	curl -s -X POST "http://$JENKINSIP:8080/createItem?name=$TESTSUITE-$TEST" -k -H "$CRUMB" --user "$USERNAME":"$USERPASS" --data-binary @job-template-config.xml -H "Content-Type:text/xml"
 	sleep 1
-	curl -s -X POST "http://$JENKINSIP:8080/job/$TESTSUITE-$TEST/buildWithParameters?delay=0sec&TESTCASE=$TEST&token=$APITOKEN" -k -H "$CRUMB" --user "$USERNAME":"$USERPASS"
+	curl -s -X POST "http://$JENKINSIP:8080/job/$TESTSUITE-$TEST/buildWithParameters?delay=0sec&TESTCASE=$TEST" -k -H "$CRUMB" --user "$USERNAME":"$USERPASS"
     fi
     sleep 1
 done <"../../../$TESTSUITE/$TESTSUITE.conf"
