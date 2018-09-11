6482eae78933a05cd2c7715a1eba751518adf764
ndebuhr <ndebuhr@protonmail.com>
2018-09-11 18:57:11 +0000

    Use internal networking and a preconfigured Hub IP for the Jenkins setup

--- docker/continuous-integration/simulate-users.sh
+++ docker/continuous-integration/simulate-users.sh
@@ -2,3 +2,6 @@ export REPOSITORY=hydroshare
 cd sim-users
 bash build-push-images.sh
 bash hub-up.sh 0 2  # 2 chrome instances
+cd ..
+HUBIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sim-users_selenium-hub_1 | sed 's/\./\\\./g')  # get Hub IP and escape periods
+find ./jenkins/jobs/ -name "*.xml" | xargs -i sed -i "s/--grid localhost/--grid $HUBIP/g" {}  # replace localhost grid setting with hub ip
